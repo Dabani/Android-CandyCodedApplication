37767b15662cda40248cdcc945f75e6cfd0f3cb0
DabaniCode <dabani.rm@gmail.com>
2020-09-29 18:05:23 +0200

    Start Phone Activity

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -42,5 +42,11 @@ public class InfoActivity extends AppCompatActivity {
     // TODO - Task 3 - Launch the Phone Activity
     // ***
 
+    public void createPhoneIntent(View view) {
+        Uri dialUri = Uri.parse("tel:0123456789");
+        Intent dialIntent = new Intent(Intent.ACTION_DIAL);
+        dialIntent.setData(dialUri);
+        startActivity(dialIntent);
+    }
 
 }
